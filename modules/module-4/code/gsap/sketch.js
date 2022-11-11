f8cf19f684cd5f3ab01edf63cd64e68673f195fe
Billy Bennett <bennett.billy@gmail.com>
2022-11-10 20:25:03 -0500

    module 4 class examples gsap

---
+++ modules/module-4/code/gsap/sketch.js
@@ -0,0 +1,40 @@
+let ball = { 
+    x: 0, 
+    y: 200,
+    col: null,
+};
+
+function setup() {
+    createCanvas(800, 400);
+    createButton('gsap.fromTo').mousePressed(moveBall)
+    createButton('gsap.to with repeat').mousePressed(moveBallChangeColor)
+
+    ball.col = color(180, 100, 212) // we have to wait for p5 to load
+}
+
+function draw() {
+    background(220);
+    fill(ball.col)
+    ellipse(ball.x, ball.y, 100)
+}
+
+function moveBall() {
+    gsap.fromTo(
+        ball, // object to change
+        { x: 0 }, // from this...
+        { x: width }, // to this...
+    );
+}
+
+function moveBallChangeColor() {
+    gsap.to(
+        ball, // object to change
+        { // go to these values...
+            x: width * 0.5,  
+            col: color(100, 200, 100),
+            duration: 0.5, // over 3 seconds
+            repeat: 6, // after the first time, how many more?
+            yoyo: true, // this will alternate back and forth on each repeat, like a yoyo
+        }
+    );
+}
